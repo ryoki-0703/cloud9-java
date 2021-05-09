fcf623be15d100debc5edf9d53b9c6f924119950
ryoki-0703 <tacchimi504@gmail.com>
2021-05-09 13:59:31 +0000

    構文練習問題1

---
+++ study1.java
@@ -0,0 +1,12 @@
+public class study1 {
+
+  public static void main(String[] args) {
+
+    String[] a = {"あいうえお", "ABC", "いか"};
+
+    System.out.println("引数は" + a.length + "個指定されています");
+    System.out.println("1番目の引数は" + a[1] + "です");
+    System.out.println("2番目の引数は" + a[0] + "です");
+    System.out.println("3番目の引数は" + a[2] + "です");
+  }
+}
